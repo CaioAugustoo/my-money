3ad1df413571ceb056835ef73d1a363280a749f2
caioaugustoo <caioamfr@gmail.com>
2021-08-24 12:34:18 -0300

    Criado modelo para transação

--- src/modules/transactionModel.ts
+++ src/modules/transactionModel.ts
@@ -2,13 +2,24 @@ import { formatDate } from "../utils/formatters/date.js";
 import { formatPrice } from "../utils/formatters/price.js";
 import { ITransaction } from "./transactions.js";
 
-export function createTransactionModel(transaction: ITransaction): string {
-  return `
-    <tr>
-      <td>${transaction.title}</td>
-      <td>${transaction.description}</td>
-      <td>${formatPrice(transaction.amount)}</td>
-      <td>${formatDate(transaction.created_at)}</td>
-    </tr>
-  `;
+export function createTransactionModel(transaction: ITransaction): Element {
+  const wrapper = document.createElement("tr");
+  const title = document.createElement("td");
+  const description = document.createElement("td");
+  const amount = document.createElement("td");
+  const date = document.createElement("td");
+
+  title.innerText = transaction.title;
+  description.innerText = transaction.description;
+  amount.innerText = formatPrice(transaction.amount);
+  date.innerText = formatDate(transaction.created_at);
+
+  amount.style.color = transaction.type === "income" ? "green" : "red";
+
+  wrapper.appendChild(title);
+  wrapper.appendChild(description);
+  wrapper.appendChild(amount);
+  wrapper.appendChild(date);
+
+  return wrapper;
 }
